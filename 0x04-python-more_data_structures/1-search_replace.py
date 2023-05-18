5fc3b7314070d7f309149e24d912cc727ec397b9
AlbertKwasond <atsukwashie@gmail.com>
2023-05-18 03:39:56 -0700

    upade

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
     new_list = search_replace(my_list, 2, 89)
     print(my_list)
-    print(new_list)i
+    print(new_list)
